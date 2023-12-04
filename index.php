c78157faf2461960dbf063a12e7ef02f3e4831c9
mclegarejos <legarejos.mcfernand@gmail.com>
2023-12-04 14:02:07 +0800

    user navbar

--- index.php
+++ index.php
@@ -8,10 +8,34 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>MY BLOG</title>
   </head>
   <body>
+
+    <!--MENU -->
+    <nav class="navbar navbar-expand-lg navbar-light mynav">
+  <a class="navbar-brand" href="index.php"><img width="100px" src="img/logo.png"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">POST<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="about.php">ABOUT ME</a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
     
+
+
+
+
 
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
